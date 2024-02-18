e0a5cf845b6c6949f7c62bdc884a4e1eabc866b3
Shamim.nrl <shamim.nrl1@gmail.com>
2024-02-19 01:48:34 +0600

    js update

---
+++ app.js
@@ -0,0 +1,74 @@
+
+
+let totalSeats = 0;
+const bookingBtn = document.querySelectorAll(".booking-btn")
+for (const iterator of bookingBtn) {
+    iterator.addEventListener('click', function (e) {
+        totalSeats = totalSeats + 1;
+
+        e.target.style.backgroundColor = '#1DD100'
+        e.target.setAttribute("disabled", 'true')
+        document.getElementById("seat-count").innerText = totalSeats;
+
+        // _______________________________________________________________________________
+        const seatsLeft = document.getElementById("seats-left").innerText
+        const left = seatsLeft - totalSeats
+        document.getElementById("seats-left").innerText = left;
+        // _________________________________________________________________________________
+        const booking2 = e.target.innerText
+        const selectedSet = document.getElementById("selected-set")
+        const p = document.createElement("p")
+        p.innerText = booking2
+        selectedSet.appendChild(p)
+        const p2 = document.createElement("p2")
+        p2.innerText = "Economy"
+        selectedSet.appendChild(p2)
+        const p3 = document.createElement("p3")
+        p3.innerText = 550
+        selectedSet.appendChild(p3)
+
+        const price = parseInt(totalSeats * 550)
+
+        document.getElementById("price").innerText = price
+
+        if (totalSeats >= 5) {
+            alert('do not more bay')
+            return
+        }
+        // discount section -----------------------------------
+        const apply = document.getElementById("apply")
+        apply.addEventListener("click", function (e) {
+
+            const input = document.getElementById("input").value
+            const coupon = input.split(" ").join("").toUpperCase()
+
+            if (coupon === "NEWS15") {
+                discount = price * 15 / 100
+                document.getElementById("total-price").innerText = price - discount
+
+            } else if (coupon === "COUPLE20") {
+                discount = price * 20 / 100
+                document.getElementById("total-price").innerText = price - discount
+            }
+            else {
+                alert("invalid")
+            }
+
+        })
+
+
+    })
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
